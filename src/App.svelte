84988d0f4adaf8ba6add8dc1bb46b6cfe9048a58
Thor Stenbæk <tst@dips.no>
2022-04-26 15:10:08 +0200

    Added Inner component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+	import Inner from "./Inner.svelte";
+
+	let m = {
+		clientX: 0,
+		clientY: 0
+	};
+
+	function handleMyEvent(e) {
+		m = e.detail;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Inner on:myevent={handleMyEvent}/>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div>
+	{m.clientX} x {m.clientY}
+</div>
---
+++ src/Inner.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function dispatchMouseMove(e){
+        dispatch('myevent', {
+            clientX: e.clientX,
+            clientY: e.clientY
+        });
+    }
+</script>
+
+<div on:mousemove={dispatchMouseMove}/>
+
+<style>
+	div {
+		background-color: darkcyan;
+		width: 640px;
+		height: 480px;
+		padding: 5px;
+	}
+</style>
+
