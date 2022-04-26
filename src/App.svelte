f2acbc8161e4c8012b0aabf29a0b4f78742a09d7
Thor Stenbæk <tst@dips.no>
2022-04-26 15:24:15 +0200

    Bind to two text boxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
 <script>
-	export let name;
+	let patient = {
+		firstName: "Thor",
+		lastName: "Stenbæk"
+	}
+
+	let fullName = "";
+
+	$: fullName = patient.firstName + " " + patient.lastName
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+
+<label>
+	Firstname:
+	<input type="text" bind:value={patient.firstName}/>
+</label>
+<label>
+	Lastname:
+	<input type="text" bind:value={patient.lastName}/>
+</label>
+
+<p>
+	Fullname: {fullName}
+</p>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	label {
+		display: flex;
 	}
 </style>
