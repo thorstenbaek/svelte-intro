294178be7e8d2d6d83f95160efc13fbbd2e4907e
Thor Stenbæk <tst@dips.no>
2022-04-26 13:58:29 +0200

    Added if else Added each

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+	var isHidden = false;
+
+	var toDos = [
+		{
+			id: 1,
+			title: 'Learn Svelte',
+			completed: false
+		},
+		{
+			id: 2,
+			title: 'Build an app',
+			completed: false
+		},
+		{
+			id: 3,
+			title: 'Deploy it to the web',
+			completed: false
+		}
+	];
+	
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h2>if else</h2>
+<button on:click={() => isHidden = !isHidden}>
+	Hit me!
+</button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<div>Some information goes here...</div>
+{#if !isHidden}
+	<div>I want to hide this when the button is hit</div>
+{:else}
+	<div>This is displayed when everything else is hidden</div>
+{/if}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+<h2>each</h2>
+<p>
+{#each toDos as toDo, i}
+	<div>
+		<input type="checkbox" value={toDo.completed}/> {i + 1}. {toDo.title}
+	</div>
+{/each}
+</p>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
