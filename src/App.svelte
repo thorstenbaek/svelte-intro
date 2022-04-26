4f9b11e3ac8cad4abdf371a0acb26fa08acc8201
Thor Stenbæk <tst@dips.no>
2022-04-26 21:37:20 +0200

    Added readable demo with time

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,16 @@
 	import DisplayPanel from './DisplayPanel.svelte';
 	import DecreasePanel from './DecreasePanel.svelte';
 	import IncreasePanel from './IncreasePanel.svelte';
-	import {global} from "./stores.js";	
+	import {global, time} from "./stores.js";	
 </script>
 
 
 <DecreasePanel/> {$global} <IncreasePanel/>
 <DisplayPanel/>
+
+<div>
+	{$time.toLocaleString()}
+</div>
 
 
 
--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,7 @@
-import {writable} from "svelte/store";
+import {writable, readable} from "svelte/store";
 
 export const global = writable(0);
 
+export const time = readable(new Date(), set => {
+    setInterval(() => {set(new Date())}, 1000);        
+});
